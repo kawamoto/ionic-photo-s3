e2c2cfc41060bee21b480422a8de80a70bf63500
Yuki Kawamoto <kawamoto62@gmail.com>
2019-06-19 16:50:50 +0900

    reload images when new image is uploaded

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { S3AlbumComponent } from 'aws-amplify-angular/dist/src/components/storage/s3-album-component/s3-album.factory';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  @ViewChild(S3AlbumComponent) album: S3AlbumComponent;
 
   constructor() {}
 
+  onImageUploaded(ev: object) {
+    this.album.loadComponent();
+  }
 }
